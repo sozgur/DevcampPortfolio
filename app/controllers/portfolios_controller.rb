412b0904ad7153cf309a7d16f606feecae4c1784
Sumeyra Ozgur <smyrozgur@gmail.com>
2019-11-30 12:20:16 -0800

    Integred new and create actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,28 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio is created"}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    puts "sadfsafas #{params}"
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
 end
