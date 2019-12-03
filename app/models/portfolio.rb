3fe588da33448a07a413102dd50a5f2d6b7d49db
Sumeyra Ozgur <smyrozgur@gmail.com>
2019-12-03 15:03:58 -0800

    Impelemted image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,5 +8,13 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    #this is not overwriting if item is nil then set this main image with ||
+    self.main_image ||= "https://placehold.it/600x400"
+    self.thumb_image ||= "https://placehold.it/350x200"
+  end
+
 
 end
