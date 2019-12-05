f81cbdd569720ec8da792c884309179b3f0b9e27
Sumeyra Ozgur <smyrozgur@gmail.com>
2019-12-05 09:31:03 -0800

    Added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
+
+  def current_user
+    super ||  OpenStruct.new(name: "Guest", first_name:"Guest", last_name: "Guest", email: "guest@example.com")
+  end
 end
