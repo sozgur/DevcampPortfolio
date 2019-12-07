3b0967badb4ca39742d4080f66ebc53cee20cb7f
Sumeyra Ozgur <smyrozgur@gmail.com>
2019-12-07 15:48:42 -0800

    Implemented authorization engine for portfolio and blogs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include DefaultPageContent
-
-  def current_user
-    super ||  OpenStruct.new(name: "Guest", first_name:"Guest", last_name: "Guest", email: "guest@example.com")
-  end
-
-
+  include CurrentUserConcern
 
 end
